94f32146b9e4845a8b342ccd23e54e4ddba30a1d
xixi <269293431@qq.com>
2019-02-27 21:07:26 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/healthmanagement/android/db/Province.java
@@ -0,0 +1,30 @@
+package com.healthmanagement.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;    //每个实体类都有的字段
+    private String provinceName;   //省的名字
+    private int provinceCode;    //省的代号
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public  int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+}
